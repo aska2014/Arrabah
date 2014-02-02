f8bcc9d1b3747e28bd346d5229a518171a8feae4
Kareem Mohamed <kareem3d.a@gmail.com>
2014-02-02 22:56:22 +0200

    Test

---
+++ app/config/arrabah/database.php
@@ -0,0 +1,20 @@
+<?php
+
+return array(
+
+
+	'connections' => array(
+
+		'mysql' => array(
+			'driver'    => 'mysql',
+			'host'      => 'localhost',
+			'database'  => 'arrabahn_master',
+			'username'  => 'arrabahn_kareem',
+			'password'  => 'NIUASDJN#34',
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
+		)
+	)
+
+);
